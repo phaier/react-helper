ee72596c350222276292434b61b426fca2d8aa59
Toshiaki Inahata <darwin49@gmail.com>
2024-10-28 00:12:52 +0900

    deprecate useHandler

--- lib/hooks/useHandler.ts
+++ lib/hooks/useHandler.ts
@@ -4,6 +4,9 @@ interface HandlerRef<T> {
   handler: (arg: T) => void | Promise<void>;
 }
 
+/**
+ * @deprecated この Hook は DependencyList に対する誤解によって作成されています。useCallback ではなく、この Hook が必要になった場合は、設計に重大な問題がある可能性があります。
+ */
 export function useHandler<T>(
   handler: (arg: T) => void | Promise<void>,
   deps: DependencyList
